e14b8a77f9363f595a58cdcb509a8066e6d37800
ManvithaAinavolu <131433904+ManvithaAinavolu@users.noreply.github.com>
2026-06-22 10:17:00 +0000

    Day3 added assertions and tested the page title

--- playwright.config.js
+++ playwright.config.js
@@ -30,6 +30,8 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    headless:false,
+    viewport:null,
     trace: 'on-first-retry',
   },
 
--- tests/Day1.spec.js
+++ tests/Day1.spec.js
@@ -1,14 +1,17 @@
 const {test} = require('@playwright/test');
-
+const {expect} = require('@playwright/test');
 test('Google Search Test with Browser Fixture',async ({browser})=>{
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto('https://www.google.com/');
-    await page.screenshot({path:'google.png'});
+    /*await page.screenshot({path:'google.png'});*/
+    console.log("The Page Title is:",await page.title());
+    expect(await page.title()).toBe("Google");
+
 });
 
 
-test('Google Search Test with Page Fixture',async ({page})=>{
+/*test('Google Search Test with Page Fixture',async ({page})=>{
     await page.goto('https://www.google.com/');
     await page.screenshot({path:'google_page.png'});
-});
+});*/
