275d199889912e5c775473f414772a1db6c1e668
ManvithaAinavolu <131433904+ManvithaAinavolu@users.noreply.github.com>
2026-06-24 16:14:11 +0000

    dropdown select

--- tests/Day3.spec.js
+++ tests/Day3.spec.js
@@ -1,4 +1,4 @@
-const {test,expect}=require('@playwright/test');
+const {test,expect,selectOption}=require('@playwright/test');
 const { loginOrangeHRM } = require('./orangeHRMHelper');
 test('login to OrangeHRM', async ({ page }) => {
   await loginOrangeHRM(page);
@@ -11,4 +11,30 @@ test('Getting Text from WebPage',async ({browser})=>{
     const text=await page.locator("//h6[text()='Dashboard']").textContent();
     console.log("The Text is:",text);
     expect(text).toBe("Dashboard");
+});
+
+test.only('Navigation to Admin Page', async ({ browser }) => {
+  const context = await browser.newContext();
+  const page= await context.newPage();
+  await loginOrangeHRM(page);
+  await page.locator("//li//a//span[text()='Admin']").click();
+  await page.locator("//label[text()='Username']/following::input[1]").fill("Admin");
+  // const dropdown1=await page.locator("//label[text()='User Role']/following::div[@tabindex='0'][1]");
+  // await dropdown1.selectOption("2");
+  // await page.locator("//input[@placeholder='Type for hints...']").fill("Manvitha");
+  // const dropdown2=await page.locator("//label[text()='Status']/following::div[@tabindex='0'][2]");
+  // await dropdown2.selectOption("1");
+  
+const userRoleDropdown = page.locator("//label[text()='User Role']/following::div[@tabindex='0'][1]");
+  await userRoleDropdown.click();
+  await page.locator("//div[@role='listbox']//span[text()='Admin']").click();
+  await page.locator("//input[@placeholder='Type for hints...']").fill("Manvitha");
+  const statusDropdown = page.locator("//label[text()='Status']/following::div[@tabindex='0'][2]");
+  await statusDropdown.click();
+  await page.locator("//div[@role='listbox']//span[text()='Enabled']").click();
+  await page.locator("//button[@type='submit']").click();
+  //await page.waitForTimeout(5000);
+  await page.pause();
+  await page.screenshot({path:'admin_page.png'});
+
 });
